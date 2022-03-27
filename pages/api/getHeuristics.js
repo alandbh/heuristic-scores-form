3149b5089ba81dd62895ca047d0589d6933ec6db
Alan Vasconcelos <alandbh@gmail.com>
2022-03-27 14:16:00 -0300

    Adding get multiple heuristics feature

--- pages/api/getHeuristics.js
+++ pages/api/getHeuristics.js
@@ -3,15 +3,23 @@ import mongo from "./service";
 export default async (req, res) => {
     const { collection } = await mongo("heuristics");
 
+    const is_array = req.query.find.includes(",");
+
+    // console.log(req.query.find.split(","));
+    // console.log(is_array);
+
     const re = req.query.find
         ? new RegExp(req.query.find, "g")
         : new RegExp(".*", "g");
 
+    const findMany = is_array
+        ? { $in: req.query.find.split(",") }
+        : { $regex: re };
     const sorting = req.query.orderBy || "slug";
 
     const heuristics = await collection
         // .find({ slug: { $regex: /^h_2/ } })
-        .find({ slug: { $regex: re } })
+        .find({ slug: findMany })
         .sort({ [sorting]: 1 })
         .limit(20000)
         .toArray();
