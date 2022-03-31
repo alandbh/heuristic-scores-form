fca570565445b1efcbe0f44e9fe06b7b776789dd
Alan Vasconcelos <alandbh@gmail.com>
2022-03-31 17:27:58 -0300

    Getting all journeys, AllPlayers and AllHeuristics OK

--- pages/evaluation.js
+++ pages/evaluation.js
@@ -2,6 +2,7 @@ import Head from "next/head";
 import styles from "../styles/Home.module.scss";
 import getData from "../services/getData";
 import { useEffect, useState } from "react";
+import { useLocalStorage } from "./api/utils";
 import PlayerSelect from "../components/Playerselect/Playerselect";
 import JourneySelect from "../components/Journeyselect/Journeyselect";
 import Header from "../components/Header/Header";
@@ -12,27 +13,46 @@ import React from "react";
 // import { Container } from './styles';
 
 function evaluation() {
-    const [players, setPlayers] = useState([]);
-    // const [allPlayers, setAllPlayers] = useState([]);
-    const [activePlayer, setActivePlayer] = useState(null);
+    const [allPlayers, setAllPlayers] = useLocalStorage("allPlayers", []);
+    const [players, setPlayers] = useLocalStorage("players", []);
+    const [activePlayer, setActivePlayer] = useLocalStorage(
+        "activePlayer",
+        null
+    );
 
-    const [journeys, setJourneys] = useState([]);
-    const [activeJourney, setActiveJourney] = useState(null);
+    const [journeys, setJourneys] = useLocalStorage("journeys", []);
+    const [activeJourney, setActiveJourney] = useLocalStorage(
+        "activeJourney",
+        null
+    );
 
-    const [heuristics, setHeuristics] = useState([]);
-    const [groups, setGroups] = useState([]);
+    const [allHeuristics, setAllHeuristics] = useLocalStorage(
+        "allHeuristics",
+        []
+    );
+    const [heuristics, setHeuristics] = useLocalStorage("heuristics", []);
+    const [groups, setGroups] = useLocalStorage("groups", []);
 
     // const [hscore, setHeuScore] = useState(null);
 
-    //Fetching all Journeys
+    /**
+     *
+     * Fetching All Data From Database
+     */
     useEffect(async () => {
+        // All Journeys
+
         setJourneys(await getData("journeys"));
-        // setActiveJourney(journeys[0]);
+
+        // Fetching all Players
+        setAllPlayers(await getData("players"));
+
+        // Fetching all Heuristics
+        setAllHeuristics(await getData("heuristics"));
     }, []);
 
     // Selecting the FIRST JOURNEY
     useEffect(() => {
-        console.log("PRIMEIRA", journeys);
         setActiveJourney(journeys[0]);
     }, [journeys]);
 
@@ -59,59 +79,50 @@ function evaluation() {
                 note,
             };
 
-            // setActivePlayer(updatedActivePlayer);
+            setActivePlayer(updatedActivePlayer);
         }
     }
 
     // Filtering Players from JOURNEY
 
-    let isFirstLoad = true;
+    // useEffect(async () => {
+    //     console.log("mudou journey");
+    //     const playersFromJourney = await getData(
+    //         "players",
+    //         "slug",
+    //         playersIdsFromJourney.join(",")
+    //     );
 
-    useEffect(async () => {
-        const playersIdsFromJourney = activeJourney
-            ? activeJourney.players
-            : [];
+    //     const playersFromJourneyWithScores = isFirstLoad
+    //         ? playersFromJourney.map((player) => {
+    //               if (objIsEmpty(player.scores)) {
+    //                   journeys.map((journey) => {
+    //                       player.scores[journey.slug] = {};
+    //                   });
+    //                   // player.scores[activeJourney.slug] = {};
+    //                   activeJourney.heuristics.map((heuristic) => {
+    //                       player.scores[activeJourney.slug][heuristic] = {
+    //                           score: 3,
+    //                           note: "nana",
+    //                       };
+    //                   });
+    //                   isFirstLoad = false;
+    //                   return player;
+    //               } else {
+    //                   isFirstLoad = false;
+    //                   return player;
+    //               }
+    //           })
+    //         : players;
 
-        console.log(
-            "mudou journey",
-            activeJourney ? activeJourney.players : ""
-        );
-        const playersFromJourney = await getData(
-            "players",
-            "slug",
-            playersIdsFromJourney.join(",")
-        );
-
-        const playersFromJourneyWithScores = isFirstLoad
-            ? playersFromJourney.map((player) => {
-                  if (objIsEmpty(player.scores)) {
-                      journeys.map((journey) => {
-                          player.scores[journey.slug] = {};
-                      });
-                      // player.scores[activeJourney.slug] = {};
-                      activeJourney.heuristics.map((heuristic) => {
-                          player.scores[activeJourney.slug][heuristic] = {
-                              score: 3,
-                              note: "nana",
-                          };
-                      });
-                      isFirstLoad = false;
-                      return player;
-                  } else {
-                      isFirstLoad = false;
-                      return player;
-                  }
-              })
-            : players;
-
-        setPlayers(playersFromJourneyWithScores);
-    }, [activeJourney]);
+    //     setPlayers(playersFromJourneyWithScores);
+    // }, [activeJourney]);
 
     // Selecting the FIRST PLAYER
-    useEffect(() => {
-        console.log("PRIMEIRO PLAYER", players);
-        setActivePlayer(players[0]);
-    }, [players]);
+    // useEffect(() => {
+    //     console.log("PRIMEIRO PLAYER", players);
+    //     setActivePlayer(players[0]);
+    // }, [players]);
 
     /**
      *
@@ -120,27 +131,27 @@ function evaluation() {
 
     //Fetching all HEURISTICS based on active JOURNEY/PLAYER
 
-    useEffect(async () => {
-        const heuristicsList = activeJourney
-            ? activeJourney.heuristics.join()
-            : "";
-        const filteredHeuristics = activeJourney
-            ? await getData("heuristics", "slug", heuristicsList)
-            : [];
-        // setAllHeuristics([]);
-        setHeuristics(filteredHeuristics);
-        // debugger;
-        // console.log(filteredHeuristics);
-    }, [activeJourney, activePlayer]);
+    // useEffect(async () => {
+    //     const heuristicsList = activeJourney
+    //         ? activeJourney.heuristics.join()
+    //         : "";
+    //     const filteredHeuristics = activeJourney
+    //         ? await getData("heuristics", "slug", heuristicsList)
+    //         : [];
+    //     // setAllHeuristics([]);
+    //     setHeuristics(filteredHeuristics);
+    //     // debugger;
+    //     // console.log(filteredHeuristics);
+    // }, [activeJourney, activePlayer]);
 
-    useEffect(() => {
-        if (heuristics.length > 0) {
-            const allGroups = heuristics.map((heuristic) => heuristic.group);
-            const uniqueGroups = [...new Set(allGroups)];
-            setGroups(uniqueGroups);
-            console.log(groups);
-        }
-    }, [heuristics, activePlayer]);
+    // useEffect(() => {
+    //     if (heuristics.length > 0) {
+    //         const allGroups = heuristics.map((heuristic) => heuristic.group);
+    //         const uniqueGroups = [...new Set(allGroups)];
+    //         setGroups(uniqueGroups);
+    //         console.log(groups);
+    //     }
+    // }, [heuristics, activePlayer]);
 
     function getHeuristicsByGroup(group) {
         const heuristicsByGroup = heuristics.filter(
@@ -192,55 +203,6 @@ function evaluation() {
                 <meta name="description" content="Heuristics collector" />
                 <link rel="icon" href="/favicon.ico" />
             </Head>
-
-            <Header className="active">
-                <JourneySelect
-                    setActiveJourney={setActiveJourney}
-                    activeJourney={activeJourney}
-                    journeys={journeys}
-                ></JourneySelect>
-                <PlayerSelect
-                    setActivePlayer={setActivePlayer}
-                    activePlayer={activePlayer}
-                    players={players}
-                ></PlayerSelect>
-            </Header>
-
-            <main>
-                <div>
-                    <section>
-                        {groups.length > 0
-                            ? groups.map((group, index) => (
-                                  <div key={index}>
-                                      <div className="sectionHeader">
-                                          <h1>{`${index + 1}. ${group}`}</h1>
-                                          <div className="sectionScore">
-                                              <span>25 of 30</span>
-                                              <img src="/minichart.png" />
-                                          </div>
-                                      </div>
-
-                                      {loopHeuristics(group)}
-                                  </div>
-                              ))
-                            : "nnnnnnnnn"}
-                    </section>
-                </div>
-
-                <pre style={{ maxWidth: 500, whiteSpace: "pre-line" }}>
-                    {activePlayer && activePlayer.scores && activeJourney
-                        ? JSON.stringify(
-                              activePlayer.scores[activeJourney.slug]
-                          )
-                        : ""}
-                </pre>
-
-                <h1 className={styles.title}>
-                    {activePlayer ? activePlayer.name : ""}
-                </h1>
-
-                <p>{activeJourney ? activeJourney.title : ""}</p>
-            </main>
         </div>
     );
 }
