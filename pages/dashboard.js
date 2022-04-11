4081a7380c5754af6efd794cd70f7dbf438b5999
Alan Vasconcelos <alandbh@gmail.com>
2022-04-11 18:30:52 -0300

    PWA

--- pages/dashboard.js
+++ pages/dashboard.js
@@ -9,9 +9,34 @@ function dashboard() {
     return (
         <>
             <Head>
-                <title>Dashboard - Heuristics Collector</title>
-                <meta name="description" content="Heuristics collector" />
-                <link rel="icon" href="/favicon.ico" />
+                <meta charset="utf-8" />
+                <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+                <meta
+                    name="viewport"
+                    content="width=device-width,initial-scale=1,minimum-scale=1,maximum-scale=1,user-scalable=no"
+                />
+                <meta
+                    name="description"
+                    content="description of your project"
+                />
+                <meta name="theme-color" content="#000" />
+                <title>Dashboard - Heuristic Collector</title>
+                <link rel="manifest" href="/manifest.json" />
+                <link rel="shortcut icon" href="/favicon.ico" />
+                <link rel="apple-touch-icon" href="/apple-icon.png"></link>
+                <link
+                    rel="preconnect"
+                    href="https://fonts.googleapis.com"
+                ></link>
+                <link
+                    rel="preconnect"
+                    href="https://fonts.gstatic.com"
+                    crossorigin
+                ></link>
+                <link
+                    href="https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,300;0,400;0,700;1,400&display=swap"
+                    rel="stylesheet"
+                ></link>
             </Head>
 
             <Header className="active"></Header>
--- pages/evaluation.js
+++ pages/evaluation.js
@@ -584,9 +584,21 @@ function evaluation() {
         check() && (
             <>
                 <Head>
-                    <title>Evaluation - Heuristics Collector</title>
-                    <meta name="description" content="Heuristics collector" />
-                    <link rel="icon" href="/favicon.ico" />
+                    <meta charset="utf-8" />
+                    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+                    <meta
+                        name="viewport"
+                        content="width=device-width,initial-scale=1,minimum-scale=1,maximum-scale=1,user-scalable=no"
+                    />
+                    <meta
+                        name="description"
+                        content="description of your project"
+                    />
+                    <meta name="theme-color" content="#000" />
+                    <title>Evaluation - Heuristic Collector</title>
+                    <link rel="manifest" href="/manifest.json" />
+                    <link rel="shortcut icon" href="/favicon.ico" />
+                    <link rel="apple-touch-icon" href="/apple-icon.png"></link>
                     <link
                         rel="preconnect"
                         href="https://fonts.googleapis.com"
